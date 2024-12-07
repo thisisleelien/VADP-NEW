109d29b344e8090825796e97a94b6244b73a9f64
DanyYap <124513296+DanyYap@users.noreply.github.com>
2024-12-07 21:35:19 +0800

    Fire_ParticleSystem

---
+++ Assets/Script/SafeZoneSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SafeZoneSystem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Civilians"))
+        {
+            Debug.Log("Save Civilians");
+        }
+    }
+}
