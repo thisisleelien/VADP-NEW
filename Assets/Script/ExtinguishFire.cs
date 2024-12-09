4b1ef1b28636ff2a6adf3ea275aa21aed2a41232
DanyYap <124513296+DanyYap@users.noreply.github.com>
2024-12-09 23:53:35 +0800

    Result_UI_Update

--- Assets/Script/ExtinguishFire.cs
+++ Assets/Script/ExtinguishFire.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class ExtinguishFire : MonoBehaviour
 {
+    public ScoreManager scoreManager;
     private void OnParticleCollision(GameObject other)
     {
         if (other.CompareTag("fire"))
         {
             other.GetComponent<ParticleSystem>().Stop();
             other.transform.GetChild(2).gameObject.SetActive(true);
+            scoreManager.AddFireExtinguish();
         }
     }
 }
